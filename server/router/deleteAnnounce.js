aa27611f8befd07234ae19ee2e1474af55ce51a1
Gilbert Elnino <gilbeltelnino@gmail.com>
2020-01-16 10:53:02 +0200

    adding delete route

--- server/router/deleteAnnounce.js
+++ server/router/deleteAnnounce.js
@@ -1,24 +1,24 @@
-// import express from 'express';
-// import verifyToken from '../helper/verifyToken';
-// import announceStorage from '../helper/announcestr';
+import express from 'express';
+import verifyToken from '../helper/verifyToken';
+import announceStorage from '../helper/announcestr';
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.delete('/api/v1/announcement/:id',verifyToken,(req,res)=>{
-//     const foundAnnounce =  announceStorage.find(ads => ads.id === parseInt(req.params.id));
-//     if(!foundAnnounce){
-//         return res.status(404).json({
-//             message: 'announcement with the given id not Found'
-//         })
-//     } else{
-//         const index = announceStorage.indexOf(foundAnnounce);
-//         announceStorage.splice(index,1)
-//         res.status(200).json({
-//                 message: 'Announcement deleted Successfully'
-//         })
-//     }
+router.delete('/api/v1/announcement/:id',verifyToken,(req,res)=>{
+    const foundAnnounce =  announceStorage.find(ads => ads.id === parseInt(req.params.id));
+    if(!foundAnnounce){
+        return res.status(404).json({
+            message: 'announcement with the given id not Found'
+        })
+    } else{
+        const index = announceStorage.indexOf(foundAnnounce);
+        announceStorage.splice(index,1)
+        res.status(200).json({
+                message: 'Announcement deleted Successfully'
+        })
+    }
 
-// })
+})
 
 
-// export default router;
+export default router;
