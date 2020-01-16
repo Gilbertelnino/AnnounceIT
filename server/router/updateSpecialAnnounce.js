5824a196cacb8e2857c5a509d5672440577cd03c
Gilbert Elnino <gilbeltelnino@gmail.com>
2020-01-16 10:49:29 +0200

    adding router  that will allow user to add information to the specific announcement

--- server/router/updateSpecialAnnounce.js
+++ server/router/updateSpecialAnnounce.js
@@ -31,30 +31,30 @@ router.patch('/api/v1/announcement/:id',verifyToken,(req,res,next)=>{
     next();
 })
 
-// router.patch('/api/v1/announcement/:id/sold',verifyToken,(req,res)=>{
-//     const announceFound = announceStorage.find(ads => ads.id === parseInt(req.params.id));
-//     const {error} = announceValidation(announceFound);
-//     if(!announceFound){
-//         return res.status(404).json({
-//             message: 'Announcement with given id Not Found'
-//         })
-//     } else if(error){
-//         return res.status(400).json({
-//             message: error.details[0].message
-//         })
-//     } else{
-//         announceFound.text = req.body.text;
-//         res.status(201).json({
-//             data: {
-//                 id: announceFound.id,
-//                 text: req.body.text,
-//                 start_date: new Date(),
-//                 end_date: new Date().setFullYear(2020, 10, 14)
-//             }
-//         })
-//     }
+router.patch('/api/v1/announcement/:id/sold',verifyToken,(req,res)=>{
+    const announceFound = announceStorage.find(ads => ads.id === parseInt(req.params.id));
+    const {error} = announceValidation(announceFound);
+    if(!announceFound){
+        return res.status(404).json({
+            message: 'Announcement with given id Not Found'
+        })
+    } else if(error){
+        return res.status(400).json({
+            message: error.details[0].message
+        })
+    } else{
+        announceFound.text = req.body.text;
+        res.status(201).json({
+            data: {
+                id: announceFound.id,
+                text: req.body.text,
+                start_date: new Date(),
+                end_date: new Date().setFullYear(2020, 10, 14)
+            }
+        })
+    }
 
-// });
+});
 
 
 
