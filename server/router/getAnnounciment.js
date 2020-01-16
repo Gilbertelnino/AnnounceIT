b168bf8d2fcc79324224f359f6dcf231cb5b766d
Gilbert Elnino <gilbeltelnino@gmail.com>
2020-01-16 11:09:45 +0200

    adding features that will allow user to view announcement using query string

--- server/router/getAnnounciment.js
+++ server/router/getAnnounciment.js
@@ -30,23 +30,16 @@ router.get('/',(req,res,next)=>{
 // })
 
 
-
-
-// router.get('/:status',(req,res,next)=>{
-//     const foundquery = announceStorage.find(ads => ads.status === req.query.status);
-//     if(!foundquery){
-//         return res.status(404).json({
-//             message: 'Not Found'
-//         })
-//     } else if(foundquery){
-//         return res.status(200).json({
-//             data: foundquery
-//         })
-//     } else{
-//         return res.status(200).json({
-//             data: announceStorage
-//         })
-//     }
-
-// })
+router.get('/:status',(req,res)=>{
+    const foundquery = announceStorage.find(ads => ads.status === req.query);
+    if(!foundquery){
+        return res.status(404).json({
+            message: 'Not Found'
+        })
+    } else{
+        return res.status(200).json({
+            data: foundquery
+        })
+    }
+})
 export default router;
